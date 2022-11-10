49a22e45032fb2c0e0caca68b0d55f1521ffbff7
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2022-11-10 11:00:17 -0300

    Problema de Cors

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-fetch(`https://lucasbelmiro.com/todos`)
+// fetch(`https://lucasbelmiro.com/todos`)
+// .then(response => {
+//     response.json()
+//     .then(data => {
+//         console.log(data);
+//     })
+// })
+
+fetch(`https://lucasbelmiro.com/save`,{
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: {
+        nome: "Everton Figueiredo",
+        email: "evertonfi@tera.com",
+        senha: "123456789"
+    }
+})
 .then(response => {
     response.json()
     .then(data => {
